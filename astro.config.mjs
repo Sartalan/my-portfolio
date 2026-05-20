bf51211d5080db0e1a2d85efb384637bddd7e063
Dante <elpinguinoalan@gmail.com>
2026-05-20 15:22:38 -0300

    v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import icon from "astro-icon";
 import { defineConfig, squooshImageService } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://sartalan.github.io',
+  base: '/my-portfolio', // Replace with your actual repo name
   integrations: [tailwind(), mdx(), icon({
     include: {
       mdi: ["*"]
